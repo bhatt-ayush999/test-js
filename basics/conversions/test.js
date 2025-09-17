c9a5a1d31c0eca3d91f7e10908de2deece8d2283
bhatt-ayush999 <bhattayush598@gmail.com>
2025-09-17 19:11:12 +0530

    conversions of datatypes

---
+++ basics/conversions/test.js
@@ -0,0 +1,17 @@
+//Nan
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = 12.3
+
+// bool
+// let score = undefined
+
+//string
+// let score = undefined
+// let score = null
+// let score = 22
+let sc = String(score)
+console.log(sc);
+console.log(typeof sc);
+
