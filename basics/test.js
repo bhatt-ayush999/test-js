8d08e5c2b1eac9ea423469529b4386fcb1d1f6c7
bhatt-ayush999 <bhattayush598@gmail.com>
2025-09-17 18:00:00 +0530

    understand datatypes
    play around let/const and var. also did comments and know about undefined

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,32 @@
-console.log("Ayush")
+// console.log("Ayush") -> ctr+/ shorcut for comments
+
+// const studentID -> shows error? -> need values
+// studentID = 231 shows error due to const dataType
+
+let mail = "bhattayush@google.com"
+mail = "himanibhatt@google.com" //yes -> changable due to dataType
+
+
+// console.table([mail,studentID]); //log -> tab shorcut for console.log and .table provides tabular form of data with  indexing
+
+// console.table([0]); //values inside -> [] not indexing
+
+//let vs var -> declaring variable but used var because of not scope freindly 
+
+// password = "113" //var?
+
+// password = "87"
+// {
+//     password = "234"
+//     console.log(password); 
+// }
+
+// {
+//     password = "123"
+//     console.log(password); 
+// }
+
+// console.log(password); 
+
+let otp //no value stores in it so it's -> undefined
+console.log(otp);
