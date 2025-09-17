f24c9b40560a68b2d73ded71f4a4ef242f7736f0
bhatt-ayush999 <bhattayush598@gmail.com>
2025-09-17 18:19:18 +0530

    datatypes-2

---
+++ basics/02_datatypes/test.js
@@ -0,0 +1,22 @@
+"use strict"; // newer version of js
+
+//alert() not in node but in browser
+
+//ensure code readability
+
+let name = "Ayush" //string
+let age = 20 //number
+let sgpa = 7.8 //number
+let boolean = true
+const temp = null //empty or stanalone value -> object
+let surname //undefiend
+
+//symbol => uniqness
+//onjects
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof sgpa);
+console.log(typeof boolean);
+console.log(typeof temp);
+console.log(typeof surname);
